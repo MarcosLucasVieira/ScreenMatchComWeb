17a0561f63d4a059e8f59bfefc3a29119b2760d2
marco <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-07-11 15:27:03 -0300

    update

--- src/main/java/br/com/visionview/visionview/VisionviewApplication.java
+++ src/main/java/br/com/visionview/visionview/VisionviewApplication.java
@@ -1,9 +1,13 @@
 package br.com.visionview.visionview;
 
+import br.com.visionview.visionview.model.InfoSerie;
 import br.com.visionview.visionview.service.ConsumoApi;
+import br.com.visionview.visionview.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.atomic.DoubleAdder;
 
 @SpringBootApplication
 public class VisionviewApplication implements CommandLineRunner {
@@ -19,5 +23,9 @@ public class VisionviewApplication implements CommandLineRunner {
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=cec54ced");
 		System.out.println(json);
 
+
+		ConverteDados conversor = new ConverteDados();
+		InfoSerie info = conversor.obterDados(json, InfoSerie.class);
+		System.out.println(info);
 	}
 }
---
+++ src/main/java/br/com/visionview/visionview/service/ConverteDados.java
@@ -0,0 +1,19 @@
+package br.com.visionview.visionview.service;
+
+import br.com.visionview.visionview.model.InfoSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
