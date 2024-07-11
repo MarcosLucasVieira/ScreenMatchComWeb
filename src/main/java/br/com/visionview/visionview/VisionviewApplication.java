817caad185c7e5e1188c1f121a791b9cdb680ce6
marco <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-07-11 14:14:38 -0300

    update

---
+++ src/main/java/br/com/visionview/visionview/VisionviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.visionview.visionview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionviewApplication.class, args);
+	}
+
+}
