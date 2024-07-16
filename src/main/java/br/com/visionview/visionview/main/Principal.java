8c26d9c510e66035c3df00f5012163b0426e9fe4
marco <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-07-16 13:07:16 -0300

    estudos dia 16/07/24

--- src/main/java/br/com/visionview/visionview/main/Principal.java
+++ src/main/java/br/com/visionview/visionview/main/Principal.java
@@ -101,6 +101,14 @@ public class Principal {
                 .filter(e -> e.getAvaliacao() > 0.0)
                 .collect(Collectors.groupingBy(Episodio::getTemporada,
                         Collectors.averagingDouble(Episodio::getAvaliacao)));
-    }
 
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e-> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade de episódios avaliados: " + est.getCount());
+    }
 }
